fe5436b3630ac403a747cfba5067f524bafb2caa
Ben Askren <ben@buildcalc.com>
2016-11-09 09:50:39 -0500

    first commit

---
+++ EltonTest/MyPage.cs
@@ -0,0 +1,25 @@
+﻿// /*******************************************************************
+//  *
+//  * MyPage.cs copyright 2016 ben, 42nd Parallel - ALL RIGHTS RESERVED.
+//  *
+//  *******************************************************************/
+using System;
+
+using Xamarin.Forms;
+
+namespace EltonTest
+{
+	public class MyPage : ContentPage
+	{
+		public MyPage()
+		{
+			Content = new StackLayout
+			{
+				Children = {
+					new Label { Text = "Hello ContentPage" }
+				}
+			};
+		}
+	}
+}
+
---
+++ iOS/AppDelegate.cs
@@ -0,0 +1,27 @@
+﻿// /*******************************************************************
+//  *
+//  * AppDelegate.cs copyright 2016 ben, 42nd Parallel - ALL RIGHTS RESERVED.
+//  *
+//  *******************************************************************/
+using System;
+using System.Collections.Generic;
+using System.Linq;
+
+using Foundation;
+using UIKit;
+
+namespace EltonTest.iOS
+{
+	[Register("AppDelegate")]
+	public partial class AppDelegate : global::Xamarin.Forms.Platform.iOS.FormsApplicationDelegate
+	{
+		public override bool FinishedLaunching(UIApplication app, NSDictionary options)
+		{
+			global::Xamarin.Forms.Forms.Init();
+
+			LoadApplication(new App());
+
+			return base.FinishedLaunching(app, options);
+		}
+	}
+}
